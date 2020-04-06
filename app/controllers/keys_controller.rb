fe9a2232b529564341259843c6aeeceb2824ccd7
NicholasZeitoun <nzeit8003@gmail.com>
2020-04-06 17:40:51 +1000

    Added copy to clipboard functionality

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -1,4 +1,6 @@
 class KeysController < ApplicationController
+  before_action :set_key, only: [:destroy, :edit, :update]
+
   def index
     @key = Key.new
     @keys = Key.all
@@ -10,7 +12,7 @@ class KeysController < ApplicationController
 
   def create
     @key = Key.new(key_params)
-    @booking.user = current_user
+    @key.user = current_user
     # @plant.user = current_user
 
     if @key.save
@@ -20,9 +22,24 @@ class KeysController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @key.update(key_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
+  def set_key
+    @key = Key.find(params[:id])
+  end
+
   def key_params
-    params.require(:key).permit(:name, :account, :password, :url)
+    params.require(:key).permit(:name, :account, :password, :url, :user_id)
   end
 end
