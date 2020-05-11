07ced0e3a6f2df273434d0f82db6d2b7c8c9f06e
NicholasZeitoun <nzeit8003@gmail.com>
2020-05-11 20:31:03 +1000

    Styled the new user registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: 'pages#home'
+  get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'keys#index'
   resources :keys
